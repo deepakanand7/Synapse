759650485fa227382e363654ed0f8ec725bf25c1
deepakanand7 <2007jaishreeshyam@gmail.com>
2026-04-08 23:17:37 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,4 @@
 const SUPABASE_URL = 'https://smhpyukftcntahshzwsx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtaHB5dWtmdGNudGFoc2h6d3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNjEyNzIsImV4cCI6MjA5MDkzNzI3Mn0.Gq_Hq-tzQwYeWDEG55_X2LhxfcB1wN1SZMEWYkJ-WKM';
-
 const { createClient } = window.supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-console.log('Supabase loaded ✅');
